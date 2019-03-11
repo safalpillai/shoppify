673e8d953cd77defe38c68f252a57e5f6370d0f1
Safal Pillai <safalpillai@gmail.com>
2019-03-12 02:45:49 +0530

    SWITCHALL ISSUE

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, AfterViewInit, ViewChild } from '@angular/core';
 import { SlimLoadingBarService } from 'ng2-slim-loading-bar';
 import { Router, Event, NavigationStart, NavigationEnd, NavigationCancel, NavigationError } from '@angular/router';
 import { select } from '@angular-redux/store';
-import { Observable } from 'rxjs';
+import { Observable, fromEvent } from 'rxjs';
+import { map, filter, tap, debounceTime, switchAll } from 'rxjs/operators';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit, AfterViewInit{
     @select() cartQuantity: Observable<number>;
     showQuantity: boolean;
+    hideSearchContainer: boolean;
+    searchLoader: boolean = false;
+    @ViewChild('searchBox') searchBox: ElementRef;
 
     constructor(private router: Router, private loadingBar: SlimLoadingBarService) {
         this.router.events.subscribe((event: Event) => this.navigationInterceptor(event));
+        this.hideSearchContainer = true;
     }
 
     ngOnInit() {
         this.cartQuantity.subscribe(count => count > 0 ? this.showQuantity = true : this.showQuantity = false);
+    }
+
+    ngAfterViewInit() {
+        const searchInputEntries$ = fromEvent(this.searchBox.nativeElement, 'keyup').pipe(
+            map((e: any) => e.target.value),
+            filter((text: string) => text.length > 1),
+            debounceTime(250),
+            tap(() => this.searchLoader = true),
+            map((query: string) => this.sendQuery(query)),
+            // switchAll()
+        );
+
+        searchInputEntries$.subscribe(data => {
+            console.log(`subscription - ${data}`);
+        });
     }
 
     navigationInterceptor(event) {
@@ -31,5 +51,16 @@ export class AppComponent implements OnInit{
         if(event instanceof NavigationCancel || event instanceof NavigationEnd || event instanceof NavigationError){
             this.loadingBar.stop();
         }
+    }
+
+    showSearch() {
+        this.hideSearchContainer = false;
+    }
+    hideSearch() {
+        this.hideSearchContainer = true;
+    }
+
+    sendQuery(query: string) {
+        console.log(`query - ${query}`);
     }
 }
