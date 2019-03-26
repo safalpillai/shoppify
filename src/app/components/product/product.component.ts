72aada040d7d6e71341bb766d1312cf9342d5ef4
Safal Pillai <safalpillai@gmail.com>
2019-03-26 15:01:06 +0530

    VERSION 2.5

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -126,6 +126,6 @@ export class ProductComponent implements OnInit {
 
     sendToLogin() {
         this.router.navigate(['/profile/login']);
-        this.notify.showInfo('Login to continue', 'Info');
+        this.notify.showError('Login to continue', 'Info');
     }
 }
