124fdf346d4ee4825e888e5e249fd3dacdd580d0
Safal Pillai <safalpillai@gmail.com>
2019-03-26 14:03:42 +0530

    VERSION 2.2

--- src/app/directives/numbers-only.directive.ts
+++ src/app/directives/numbers-only.directive.ts
@@ -7,25 +7,12 @@ export class NumbersOnlyDirective {
     //numbers only 
     constructor(private el: ElementRef) { }
     
-    @HostListener('keydown', ['$event']) onKeyDown(e) {
-        if ([46, 8, 9, 27, 13, 110].indexOf(e.keyCode) !== -1 ||
-            // Allow: Ctrl+A
-            (e.keyCode === 65 && (e.ctrlKey || e.metaKey)) ||
-            // Allow: Ctrl+C
-            (e.keyCode === 67 && (e.ctrlKey || e.metaKey)) ||
-            // Allow: Ctrl+V
-            (e.keyCode === 86 && (e.ctrlKey || e.metaKey)) ||
-            // Allow: Ctrl+X
-            (e.keyCode === 88 && (e.ctrlKey || e.metaKey)) ||
-            // Allow: home, end, left, right
-            (e.keyCode >= 35 && e.keyCode <= 39)) {
-                // let it happen, don't do anything
-            return;
-        }
-        // Ensure that it is a number and stop the keypress
-        if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
-            e.preventDefault();
+    @HostListener('input', ['$event']) onKeyDown(e) {
+        let regex = /^[0-9\.]$/;
+        let str = this.el.nativeElement.value;
+        let subStr = str.substr(str.length - 1);
+        if(!regex.test(subStr)) {
+            str.length > 0 ? this.el.nativeElement.value = str.substr(0, (str.length - 1)) : this.el.nativeElement.value;
         }
     }
-
 }
